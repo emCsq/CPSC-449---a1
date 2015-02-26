b73b7a694256051928f01772f2c3af54e1d46b93
emCsq <emcsq@live.ca>
2015-02-26 11:15:25 -0700

    Just wanted to make sure it compiled. lol me picky XP

--- parser.java
+++ parser.java
@@ -18,9 +18,9 @@ public class parser extends Commands {
 			System.out.println("Qualifier --help (-h, -?) should not appear with any command-line arguments.");
 			synopsis();
 		} else if (args[0].equals("-v") || args[0].equals("--verbose")) {
-			System.out.println("SOMETHING TO DO LATER ON");
-			if (/* the args[1] (aka the jar file) is valid */) {
-				if (/* valid, check if the args[2] (aka the class) is valid */) {
+			System.out.println("SOMETHING TO DO LATER ON");			//TEMPORARY
+			if (true) {			/* the args[1] (aka the jar file) is valid */
+				if (true) {		/* valid, check if the args[2] (aka the class) is valid */
 					/* if args[2] is valid, ~ENTER COMPILER PROGRAM~
 					MORE TO DO HERE!! */
 				} else {
