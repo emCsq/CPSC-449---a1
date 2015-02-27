115425c90c1e4ace31440255387a240e69e82f5b
emCsq <emcsq@live.ca>
2015-02-26 17:42:17 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class main {
+
+	boolean keepRunningParser = true;
+	String userInput;
+	
+	Scanner input = new Scanner(System.in);
+	
+	public void mainMenu() {
+		startUp();
+		while (keepRunningParser == true) {
+			System.out.print(">");
+			userInput = input.next();
+			if (userInput.equals("q")) {
+				keepRunningParser = false;
+			}
+		}
+
+	}
+
+	public void startUp() {
+		//this is the additional last paragraph
+		System.out.println("q \t : Quit the program.\n"
+				+ "v \t : Toggle verbose mode (stack traces).\n"
+				+ "f \t : List all known functions.\n"
+				+ "? \t : Print this helpful text.\n"
+				+ "<expression>: Evaluate the expression."
+				+ "Expressions can be integers, floats, strings (surrounded in double quotes) or function\n"
+				+ "calls of the form '(identifier {expression}*)'");
+		return;
+	} 
+    
+	
+}
