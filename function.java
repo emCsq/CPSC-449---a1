ea996e8da965218c0af67fd01928f0e98e30485a
shellyginelle <sgsicat@ucalgary.ca>
2015-02-26 19:05:36 -0700

    Create function.java

---
+++ function.java
@@ -0,0 +1,25 @@
+
+public interface Functions {
+
+	public String add (String a, String b);
+	public String add (float a, float b);
+	public String add (int a, int b);
+	
+	public String sub (float a, float b);
+	public String sub (int a, int b);
+	
+	public String div (int a, int b);
+	public String div (float a, float b);
+	
+	public String mul (int a, int b);
+	public String mul (float a, float b);
+	
+	public String inc (int a);
+	public String inc (float a);
+	
+	public String dec (int a);
+	public String dec (float a);
+	
+	public String len (int a);
+
+}
