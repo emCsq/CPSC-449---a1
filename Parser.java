0ab463eba7bfb29b27646e3267acf9be61399cc4
shellyginelle <sgsicat@ucalgary.ca>
2015-03-13 15:34:10 -0600

    try catch for Errors added

--- Parser.java
+++ Parser.java
@@ -1,7 +1,11 @@
 package a2;
 
 import java.util.*;
+import java.io.File;
+import java.io.IOException;
 import java.lang.*;
+import java.lang.reflect.Method;
+import java.net.*;
 import java.text.*;
 
 public class Parser {
@@ -9,70 +13,84 @@ public class Parser {
 	private static final Class[] parameters = new Class[] {URL.class};
 
 	public static void main (String [] args){
-    
 		InCompiler mainStuff = new InCompiler();
-		char[] inputAsArray;
-
-		if (args.length == 0) {
-			synopsis();
-		} else if ((args[0].equals("-h") || args[0].equals("-?") || args[0].equals("--help")) && (args.length == 1)) {
-			synopsis();
-			System.out.println("\nThis program interprets commands of the format '(<method> {arg}*}' on the command line, finds corresponding methods in <class-name>, and executes them, printing the result to sysout. Terminate with ^D or \"exit\"");
-		} else if ((args[0].equals("-h") || args[0].equals("-?") || args[0].equals("--help")) && (args.length != 1)) {
-			System.out.println("Qualifier --help (-h, -?) should not appear with any command-line arguments.");
-			synopsis();
-		} else if (args[0].equals("-v") || args[0].equals("--verbose")) {
-			Flags.setVerbose(true); //sets verbose mode in action
-			System.out.println("Running in verbose mode...");		
-			if ((args[1].contains(".jar"))) {			/* the args[1] (aka the jar file) is valid */
-				if (true) {			/* valid, check if the args[2] (aka the class) is valid */
-					/* if args[2] is valid, ~ENTER COMPILER PROGRAM~
-					MORE TO DO HERE!! */
-					
-										/*	To load a class, it must be in the classpath
-					Tell the class loader to add your new jar file to the classpath
-					However, class loader deals with URL
-						Therefore, we need to do the following conversion:
-							.jar -> URI -> URL -> classloader
-					1) There exists a method to convert .jar -> URI
-					2) There exists a method to convert URI -> URL
-					3) Use the URL to put into classloader!
-					*/
-				// Source: http://stackoverflow.com/questions/60764/how-should-i-load-jars-dynamically-at-runtime	
-				File f = new File(args[1]);
-				URL url = f.toURI().toURL();
-				URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-				Class<?> sysclass = URLClassLoader.class;
-			        try {
-			            Method method = sysclass.getDeclaredMethod("addURL", parameters);
-			            method.setAccessible(true);
-			            method.invoke(sysloader, new Object[] {url});
-			        } catch (Throwable t) {
-			            t.printStackTrace();
-			            throw new IOException("Error, could not add URL to system classloader");
-			        }
-					
-					//We can assume that our .jar file is in the classpath for us to use and have access to
-					
-					mainStuff.mainMenu();
+		
+		try {
+			Scanner input = new Scanner(System.in);	
+			char[] inputAsArray = null;
+	
+			if (args.length == 0) {
+				synopsis();
+			} else if ((args[0].equals("-h") || args[0].equals("-?") || args[0].equals("--help")) && (args.length == 1)) {
+				synopsis();
+				System.out.println("\nThis program interprets commands of the format '(<method> {arg}*}' on the command line, finds corresponding methods in <class-name>, and executes them, printing the result to sysout. Terminate with ^D or \"exit\"");
+			} else if ((args[0].equals("-h") || args[0].equals("-?") || args[0].equals("--help")) && (args.length != 1)) {
+				System.out.println("Qualifier --help (-h, -?) should not appear with any command-line arguments.");
+				synopsis();
+			} else if (args[0].equals("-v") || args[0].equals("--verbose")) {
+				Flags.setVerbose(true); //sets verbose mode in action
+				System.out.println("Running in verbose mode...");		
+				if ((args[1].contains(".jar"))) {			/* the args[1] (aka the jar file) is valid */
+					if (true) {			/* valid, check if the args[2] (aka the class) is valid */
+						/* if args[2] is valid, ~ENTER COMPILER PROGRAM~
+						MORE TO DO HERE!! */
+						
+											/*	To load a class, it must be in the classpath
+						Tell the class loader to add your new jar file to the classpath
+						However, class loader deals with URL
+							Therefore, we need to do the following conversion:
+								.jar -> URI -> URL -> classloader
+						1) There exists a method to convert .jar -> URI
+						2) There exists a method to convert URI -> URL
+						3) Use the URL to put into classloader!
+						*/
+					// Source: http://stackoverflow.com/questions/60764/how-should-i-load-jars-dynamically-at-runtime	
+					File f = new File(args[1]);
+					URL url = f.toURI().toURL();
+					URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+					Class<?> sysclass = URLClassLoader.class;
+				        try {
+				            Method method = sysclass.getDeclaredMethod("addURL", parameters);
+				            method.setAccessible(true);
+				            method.invoke(sysloader, new Object[] {url});
+				        } catch (Throwable t) {
+					    Errors.beginError();	
+					    Errors.offsetPoint();				        	
+				            t.printStackTrace();
+				            throw new IOException("Error, could not add URL to system classloader");
+				        }
+						
+						//We can assume that our .jar file is in the classpath for us to use and have access to
+						
+						mainStuff.mainMenu();
+					} else {
+						System.out.println("Could not find class: '" + inputAsArray[2]);
+						synopsis();
+					}
 				} else {
-					System.out.println("Could not find class: '" + inputAsArray[2]);
+					System.out.println("Could not find jar file: '" + inputAsArray[1]);
 					synopsis();
 				}
 			} else {
-				System.out.println("Could not find jar file: '" + inputAsArray[1]);
-				synopsis();
+				inputAsArray = args[0].toCharArray();
+				if (Character.valueOf(inputAsArray[0]) == '-') {
+					System.out.println("Unrecognized qualifier '" + inputAsArray[1] + "' in '" + args[0] + "'");
+					synopsis();
+				} else {
+					System.out.println("Unrecognized qualifier '" + inputAsArray[0] + "' in '" + args[0] + "'");
+					synopsis();
+				}
 			}
-		} else {
-			inputAsArray = args[0].toCharArray();
-			if (Character.valueOf(inputAsArray[0]) == '-') {
-				System.out.println("Unrecognized qualifier '" + inputAsArray[1] + "' in '" + args[0] + "'");
-				synopsis();
-			} else {
-				System.out.println("Unrecognized qualifier '" + inputAsArray[0] + "' in '" + args[0] + "'");
-				synopsis();
-			}
+			
+		} catch (Throwable e1){
+			//get stack trace
+			Errors.beginError();	
+			Errors.offsetPoint();
+			e1.printStackTrace(System.out);
+			mainStuff.mainMenu();
 		}
+		
+	mainStuff.mainMenu();
 		
     }
 	
