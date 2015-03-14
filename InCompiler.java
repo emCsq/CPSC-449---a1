03f676690c2050aa30481db2699e0bef75521ea3
emCsq <emcsq@live.ca>
2015-03-14 02:02:40 -0600

    PARSE TREES FINALLY WORK!!!!
    IT'S 2 AM AND IT FINALLY WORKS MWAHAHAHAHAHAHAHA 
    for simplicity + readability's sake I've placed the processing/evaluating methods in another class called "ProcessMethods".
    
    More details on what needs to be done in the email. I'll be commenting this in the afternoon or something.

--- InCompiler.java
+++ InCompiler.java
@@ -1,19 +1,20 @@
+package a2;
+
 import java.util.Scanner;
 
 public class InCompiler {
 
 	boolean keepRunningParser = true;
 	String userInput = "";
+	String finalAnswer;
 	boolean validBrackets;
 	
 	Scanner input = new Scanner(System.in);
 
-	public Trees tree;
-	public Functions function;
+	public ProcessMethods methods;
 
 	public InCompiler() {
-		tree = new Trees();
-		function = new Functions();
+		methods = new ProcessMethods();
 	}
 	
 	
@@ -43,7 +44,9 @@ public class InCompiler {
 				if (validBrackets == false) {
 					System.out.println("Matching braces error.");
 				} else {
-					this.checkNotQuitInput(userInput);
+					//this.checkNotQuitInput(userInput);
+					finalAnswer = this.treeTime(userInput);
+					System.out.println(finalAnswer);
 				}
 			}
 		}
@@ -69,72 +72,63 @@ public class InCompiler {
 				}
 			}
 		}
+		if (balancedBraces != 0) {
+			return false;
+		}
 		return true;
 	}
 	
-	public void checkNotQuitInput(String userIn) {
-				//if verbose is on, track code
-		if (Flags.verboseOn) {
-			System.out.println("<<< checkNotQuitInput() >>>");
-		}
+	public String treeTime(String userIn) {
+		char[] userInAsArray = userIn.toCharArray();
+		char[] newArray = null;
+		String bleh;
+		boolean arrayStillContainsBraces = true;
+		boolean youreNotFinishedYet = true;
 		
-		String concatResult = "";
-		int stringLength = -9999999;
-		int intResult = -9999999;
-		float floatResult = -9999999;
-		boolean arg1_Integer, arg2_Integer, arg1_Float, arg2_Float, arg1_String, arg2_String;
-		
-		String[] inArray = userIn.split(" ");
-
-		//NOTE: inArray args are tentative in place of the legit arguments that will be from the tree	
-		if (inArray.length == 3) {		//function arg1 arg2
-			arg1_Integer = tree.isInteger(inArray[1]);	//check if arg1 is an Integer
-			arg2_Integer = tree.isInteger(inArray[2]);	//check if arg2 is an Integer 
-			arg1_Float = tree.isFloat(inArray[1]);		//check if arg1 is a Float
-			arg2_Float = tree.isFloat(inArray[2]);		//check if arg2 is a Float
-			arg1_String = tree.isString(inArray[1]);	//check if arg1 is a String
-			arg2_String = tree.isString(inArray[2]);	//check if arg2 is a String 	
-
-			if ((arg1_Integer == true) && (arg2_Integer == true)) {
-				intResult = this.processTwoIntegers(inArray[0], Integer.parseInt(inArray[1]), Integer.parseInt(inArray[2]));
-			} else if ((arg1_Float == true) && (arg2_Float == true)) {
-				floatResult = this.processTwoFloats(inArray[0], Float.parseFloat(inArray[1]), Float.parseFloat(inArray[2]));
-			} else if (((arg1_Float == true) && (arg2_Integer == true)) || ((arg1_Integer == true) && (arg2_Float == true))) {
-				System.out.println("Incorrect argument types.");
-			} else if ((arg1_String == true) && (arg2_String == true)) {
-				concatResult = this.processStringConcat(inArray[1], inArray[2]);
-			} else {
-				System.out.println("Unexpected character encountered at offset."); //temp
+		//System.out.println("Input is: " + userIn);
+		for (int i = 0; i < userInAsArray.length; i++) {	
+			if (userInAsArray[i] == '(') {
+				bleh = (new String(userInAsArray, 0, i) + this.treeTime(new String(userInAsArray, i+1, userInAsArray.length-(i+1))));
+				//bleh = this.treeTime(new String(userInAsArray, i+1, userInAsArray.length-(i+1)));
+				//System.out.println("Result this time is " + bleh);
+				
+				newArray = bleh.toCharArray();
+				arrayStillContainsBraces = arrayHasBraces(newArray);
+				youreNotFinishedYet = arrayHasSpaces(newArray);
+				//System.out.println("bleh is actually " + bleh);
+				if ((arrayStillContainsBraces != true) || (youreNotFinishedYet != true)) {
+					//System.out.println("DONE. Result is " + bleh);
+					return bleh;
+				} else {
+					return treeTime(bleh);
+				}
+			} else if (userInAsArray[i] == ')') {
+				String thingToProcess = new String(userInAsArray, 0, i);
+				//System.out.println("ThingToProcess = " + thingToProcess);
+				String result = methods.timeToEvaluateStuff(thingToProcess);
+				return result + (new String(userInAsArray, i+1, userInAsArray.length-(i+1)));
 			}
-		} else if (inArray.length == 2) {	//function arg1
-			arg1_Integer = tree.isInteger(inArray[1]);
-			arg1_Float = tree.isFloat(inArray[1]);
-			arg1_String = tree.isString(inArray[1]);
-			
-			if (arg1_Integer == true) {
-				intResult = this.processOneInteger(inArray[0], Integer.parseInt(inArray[1]));
-			} else if (arg1_Float == true) {
-				floatResult = this.processOneFloat(inArray[0], Float.parseFloat(inArray[1]));
-			} else if ((inArray[0].compareTo("len") == 0) && arg1_String == true) {
-				stringLength = this.processStringLength(inArray[1]);
-			} else {
-				System.out.println("Unexpected character encountered at offset."); //temp
-			}
-		} else {
-			System.out.println("FAIL"); //temp
 		}
+		return null;
+	}
 	
-		//print out the FINAL result 
-		if (intResult != -9999999) {
-			System.out.println(intResult);
-		} else if (floatResult != -9999999) {
-			System.out.println(floatResult);	
-		}else if (concatResult != "") {
-			System.out.println(concatResult);
-		} else if (stringLength != -9999999) {
-			System.out.println(stringLength);
+	
+	public boolean arrayHasBraces(char[] inArray) {
+		for (int i=0; i<inArray.length; i++) {
+			if ((inArray[i] == '(') || (inArray[i] == ')')) {
+				return true;
+			}
 		}
-		
+		return false;
+	}
+	
+	public boolean arrayHasSpaces(char[] inArray) {
+		for (int i=0; i<inArray.length; i++) {
+			if((inArray[i] == ' ')) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public void startUp() {
@@ -165,92 +159,5 @@ public class InCompiler {
 				+ "(dec float) : float\n"
 				+ "(len string) : int\n");
 	}
-	
-	public int processTwoIntegers(String funct, int arg1, int arg2) {
-		//if verbose is on, track code
-		if (Flags.verboseOn) {
-			System.out.println("<<< processTwoIntegers() >>>");
-		}		
-		
-		if (funct.compareTo("add") == 0) {
-			return function.add(arg1, arg2);
-		} else if (funct.compareTo("sub") == 0) {
-			return function.sub(arg1, arg2);
-		} else if (funct.compareTo("div") == 0) {
-			return function.div(arg1, arg2);
-		} else if (funct.compareTo("mul") == 0) {
-			return function.mul(arg1, arg2);
-		} else {
-			return -1;
-		}
-	}
-	
-	public int processOneInteger(String funct, int arg1) {
-		//if verbose is on, track code
-		if (Flags.verboseOn) {
-			System.out.println("<<< processOneInteger() >>>");
-		}
-		
-		if (funct.compareTo("inc") == 0) {
-			return function.inc(arg1);
-		} else if (funct.compareTo("dec") == 0) {
-			return function.dec(arg1);
-		} else {
-			return -1;
-		}
-	}	
-	
-	public float processTwoFloats(String funct, float arg1, float arg2) {
-		//if verbose is on, track code
-		if (Flags.verboseOn) {
-			System.out.println("<<< processTwoFloats() >>>");
-		}
-		
-		if (funct.compareTo("add") == 0) {
-			return function.add(arg1, arg2);
-		} else if (funct.compareTo("sub") == 0) {
-			return function.sub(arg1, arg2);
-		} else if (funct.compareTo("div") == 0) {
-			return function.div(arg1, arg2);
-		} else if (funct.compareTo("mul") == 0) {
-			return function.mul(arg1, arg2);
-		} else {
-			return -1;
-		}
-	}
-	
-	public float processOneFloat(String funct, float arg1) {
-		//if verbose is on, track code
-		if (Flags.verboseOn) {
-			System.out.println("<<< processOneFloat() >>>");
-		}
-		
-		if (funct.compareTo("inc") == 0) {
-			return function.inc(arg1);
-		} else if (funct.compareTo("dec") == 0) {
-			return function.dec(arg1);
-		} else {
-			return -1;
-		}
-	}	
-	
-	
-	public String processStringConcat(String arg1, String arg2) {
-		//if verbose is on, track code
-		if (Flags.verboseOn) {
-			System.out.println("<<< processStringConcat() >>>");
-		}
-		
-		return function.add(arg1, arg2);
-	}
-	
-	public int processStringLength(String arg1) {
-		//if verbose is on, track code
-		if (Flags.verboseOn) {
-			System.out.println("<<< processStringLength() >>>");
-		}
-		
-		return function.len(arg1);
-	}
-	
+
 }
