0f08ac50bacf6b5011a18639ee99df50887a5ed7
emCsq <emcsq@live.ca>
2015-03-20 00:37:31 -0600

    COMPLETE (aka do not touch)

--- LoadJarClass.java
+++ LoadJarClass.java
@@ -6,17 +6,25 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
-/*
- * Purpose of class:
- * Loads .jar file so that classes can be accessed from it
- * Loads classes that exist in the .jar file
- * 	Loaded class can be used to access the methods within that class
- */
-
 public class LoadJarClass {
 	
 	private Class<?> loadedClass;
 	
+	/**
+	 * Loads .jar file so that classes can be accessed from it
+	 * Loads classes that exist in the .jar file
+	 * Loaded class can be used to access the methods within that class
+	 * 
+	 * @param fileName name of the jar file
+	 * @param className name of the class within the jar file
+	 * @throws MalformedURLException
+	 * @throws ClassNotFoundException
+	 * @throws NoSuchMethodException
+	 * @throws SecurityException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 * @throws InvocationTargetException
+	 */
 	public void loadJC(String fileName, String className) throws MalformedURLException, ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		
 		String fileStrName = fileName;
@@ -30,8 +38,11 @@ public class LoadJarClass {
 		loadedClass = cLoader.loadClass(classStrName);	
 	}
 	
-	//Access to the loadedClass instance
-	//Used to get the methods from the loaded class
+	/**
+	 * Provides access to the loadedClass instance. 
+	 * Purpose is to get the methods from the loaded class
+	 * @return methods from the loaded class
+	 */
 	public Class<?> getLoadedClass() {
 		return loadedClass;
 	}
