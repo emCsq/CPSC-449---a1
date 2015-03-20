163f593ce33380c7fcf85583bbb5e9902d3da975
emCsq <emcsq@live.ca>
2015-03-20 01:12:31 -0600

    COMPLETE (aka do not touch)

--- Flags.java
+++ Flags.java
@@ -9,15 +9,21 @@ public class Flags {
 	
 	public static boolean verboseOn = false;
 
-	//This gets the status of verboseOn
+	/**
+	 * This gets the status of verboseOn.
+	 * @return the status of verboseOn; a boolean value.
+	 */
 	public static boolean getVerbose() {
 		return (verboseOn);
 	}
 
-	/*
-	This will set when the user inidicates its use, all if not most classes 
-	have verbose messages in each function to show its execution track
-	*/
+	
+	/**
+	 * This will set the boolean value of whether verbose mode is on or not
+	 * when the user indicates its use. All if not most classes will 
+	 * have verbose messages in each function to show its execution track.
+	 * @param onValue the boolean value of whether verbose mode is on or not.
+	 */
 	public static void setVerbose(boolean onValue) {
 		verboseOn = onValue;
 	}
