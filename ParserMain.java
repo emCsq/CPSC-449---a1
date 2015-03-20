1b0b22529f11497feb5e42024bd102aea6de0369
jkdelima <jkp_d5@yahoo.com>
2015-03-20 00:31:58 -0600

    Adding jar name and class name checks

--- ParserMain.java
+++ ParserMain.java
@@ -21,24 +21,49 @@ public class ParserMain {
 			} else if ((args[0].equals("-h") || args[0].equals("-?") || args[0].equals("--help")) && (args.length != 1)) {
 				System.out.println("Qualifier --help (-h, -?) should not appear with any command-line arguments.");
 				synopsis();
-			} else if (args[0].equals("-v") || args[0].equals("--verbose")) {
-				Flags.setVerbose(true); //sets verbose mode in action
+			} else if (((args[0].equals("-v") || args[0].equals("--verbose")) && args[1].contains(".jar") && args.length == 3)
+					|| (args[0].equals("-v") || args[0].equals("--verbose")) && args[1].contains(".jar") && args.length == 2){
+				Flags.setVerbose(true);
 				System.out.println("Running in verbose mode...");		
-				if ((args[1].contains(".jar"))) { /* TO ADD HERE - Kyle's stuff!! // the args[1] (aka the jar file) is valid */
-					if (true) {			/* valid, check if the args[2] (aka the class) is valid */
-						/* if args[2] is valid, ~ENTER COMPILER PROGRAM~ */
-					
-						//This will load the .jar file from arg[1] and load the classes from arg[2] in the .jar file
-						load.loadJC(args[1], args[2]);
-					
-						mainStuff.mainMenu();
+				if (jarValid(args[1])) { // Check if jar file is valid
+					if (args.length == 2) {  // Check if defaulted "Commands" class is in the jar file
+						if (classValid(args[1], "Commands")) {
+							load.loadJC(args[1], "Commands");
+							mainStuff.mainMenu();
+						} else {
+							System.out.println("Could not find class: Commands");
+						}
 					} else {
-						System.out.println("Could not find class: '" + inputAsArray[2]);
-						synopsis();
+						if (classValid(args[1], args[2])) { // Check if class argument is in the jar file
+							load.loadJC(args[1], args[2]);
+							mainStuff.mainMenu();
+						} else {
+							System.out.println("Could not find class: " + args[2]);
+						}
 					}
 				} else {
-					System.out.println("Could not find jar file: '" + inputAsArray[1]);
-					synopsis();
+					System.out.println("Could not find jar file: " + args[1]);
+				}
+			} else if ((args[0].contains(".jar") && args.length == 2) || (args[0].contains(".jar") && args.length == 1)) {
+				Flags.setVerbose(false);
+				if (jarValid(args[0])) { // Check if jar file is valid
+					if (args.length == 1) { // Check if defaulted "Commands" class is in the jar file
+						if (classValid(args[0], "Commands")) {
+							load.loadJC(args[0], "Commands");
+							mainStuff.mainMenu();
+						} else {
+							System.out.println("Could not find class: Commands");
+						}
+					} else {
+						if (classValid(args[0], args[1])) { // Check if class argument is in the jar file
+							load.loadJC(args[0], args[1]);
+							mainStuff.mainMenu();
+						} else {
+							System.out.println("Could not find class: " + args[1]);
+						}
+					}
+				} else {
+					System.out.println("Could not find jar file: " + args[0]);
 				}
 			} else {
 				inputAsArray = args[0].toCharArray();
@@ -109,4 +134,55 @@ public class ParserMain {
 				+ "\t at Method.main(Methods.blah)");
 		return; 
 	}
+	
+	/**
+	 * Checks if jar file is in the class path and is valid
+	 * @param jarName is the name of the jar file
+	 * @return true if jar file is acceptable, false otherwise
+	 * Based on one of the answers from http://stackoverflow.com/questions/20152195/how-to-check-if-a-jar-file-is-valid
+	 */
+	public static boolean jarValid(String jarName) {
+		try {
+	        JarFile jarFile = new JarFile(jarName);
+	        Enumeration<? extends ZipEntry> e = jarFile.entries();
+	        while(e.hasMoreElements()) {
+	            ZipEntry entry = e.nextElement();
+	            //System.out.println(entry.getName());
+	        }
+	        return true;
+	    } catch(Exception ex) {
+	        return false;
+	    }
+	}
+	
+	/**
+	 * Checks if class is in the jar file and is valid
+	 * @param jarName is the name of the jar file, className is the name of the class
+	 * @return true if class is acceptable, false otherwise
+	 * Based on one of the answers from http://stackoverflow.com/questions/11016092/how-to-load-classes-at-runtime-from-a-folder-or-jar
+	 */
+	public static boolean classValid(String jarName, String className) {
+		try {
+			JarFile jarFile = new JarFile(jarName);
+			Enumeration e = jarFile.entries();
+			URL[] urls = { new URL("jar:file:" + jarName + "!/") };
+			URLClassLoader cl = URLClassLoader.newInstance(urls);
+		    while (e.hasMoreElements()) {
+		        JarEntry je = (JarEntry) e.nextElement();
+		        if(je.isDirectory() || !je.getName().endsWith(".class")){
+		            continue;
+		        }
+		        // -6 because of .class
+		        String classes = je.getName().substring(0,je.getName().length()-6);
+		        classes = classes.replace('/', '.');
+		        if(classes.equals(className)) {
+		        	return true;
+		        }
+		    }
+		    return false;
+		} catch(Exception ex) {
+			return false;
+		}
+	}
+	
 }
