3a2e1e8db3096798f526ea8d9d70ceb711ed9ea4
shellyginelle <sgsicat@ucalgary.ca>
2015-03-19 20:10:11 -0600

    Added Documentation & Took out unused code - Shelly

--- Flags.java
+++ Flags.java
@@ -8,23 +8,18 @@ verbose mode or has encountered a non-fatal error.
 public class Flags {
 	
 	public static boolean verboseOn = false;
-	public static boolean errorOn = false;
 
+	//This gets the status of verboseOn
 	public static boolean getVerbose() {
 		return (verboseOn);
 	}
 
+	/*
+	This will set when the user inidicates its use, all if not most classes 
+	have verbose messages in each function to show its execution track
+	*/
 	public static void setVerbose(boolean onValue) {
 		verboseOn = onValue;
 	}
-	
-	public static boolean getError() {
-		return (errorOn);
-	}
-
-	public static void setError(boolean onValue) {
-		errorOn = onValue;
-	}
-	
 
 }
